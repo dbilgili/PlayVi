cf7026138549c91bc97e264779fd6e4ac5640b09
dbilgili <dbilgili@sabanciuniv.edu>
2019-03-11 22:14:57 +0100

    CSSTransitions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import FrontPage from './components/views/FrontPage';
 import CreateParty from './components/views/CreateParty';
 import JoinParty from './components/views/JoinParty';
@@ -13,9 +14,30 @@ const App = () => {
 
   return (
     <div className="App">
-      {screen === 'frontpage' && <FrontPage screen={type => setScreen(type)} />}
-      {screen === 'create' && <CreateParty screen={type => setScreen(type)} />}
-      {screen === 'join' && <JoinParty screen={type => setScreen(type)} />}
+      <CSSTransition
+        in={screen === 'frontpage'}
+        timeout={350}
+        classNames="front-page-container"
+        unmountOnExit
+      >
+        <FrontPage screen={type => setScreen(type)} />
+      </CSSTransition>
+      <CSSTransition
+        in={screen === 'create'}
+        timeout={350}
+        classNames="front-page-container"
+        unmountOnExit
+      >
+        <CreateParty screen={type => setScreen(type)} />
+      </CSSTransition>
+      <CSSTransition
+        in={screen === 'join'}
+        timeout={350}
+        classNames="front-page-container"
+        unmountOnExit
+      >
+        <JoinParty screen={type => setScreen(type)} />
+      </CSSTransition>
     </div>
   );
 };
