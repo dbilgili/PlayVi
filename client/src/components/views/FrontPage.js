506519aed92ade74cdf2afbd0353964946b714ec
dbilgili <dbilgili@sabanciuniv.edu>
2019-03-31 23:09:52 +0200

    Fix typo

--- client/src/components/views/FrontPage.js
+++ client/src/components/views/FrontPage.js
@@ -8,7 +8,7 @@ const FrontPage = (props) => {
     <div className="front-page-container">
       <img alt="logo" className="logo" src={logo} />
       <button type="button" className="big-text-button" onClick={() => screen('create')}>Start a party</button>
-      <button type="button" className="big-text-button" onClick={() => screen('join')}>Joint to a party</button>
+      <button type="button" className="big-text-button" onClick={() => screen('join')}>Join to a party</button>
       <button type="button" className="small-text-button">About</button>
     </div>
   );
