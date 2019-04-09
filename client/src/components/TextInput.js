98810da8ba6bd3c9cc50df893bfd18acf91364a8
dbilgili <dbilgili@sabanciuniv.edu>
2019-04-09 23:09:42 +0200

    Add maxlength defaultProp

--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const TextInput = (props) => {
-  const { placeholder, getField, validate, pattern, alert } = props;
+  const { placeholder, getField, validate, pattern, maxlength, alert } = props;
   const [input, setInput] = useState('');
 
   useEffect(() => {
@@ -12,13 +12,17 @@ const TextInput = (props) => {
   return (
     <input
       type="text"
-      maxLength="15"
+      maxLength={maxlength}
       pattern={pattern}
       className={alert ? 'custom-text-input alert' : 'custom-text-input'}
       placeholder={placeholder}
       onChange={e => setInput(e.target.value)}
     />
   );
+};
+
+TextInput.defaultProps = {
+  maxLength: 15,
 };
 
 export default TextInput;
