ff4f685775ba8fc33ba80fec751afc62465aa5d6
dbilgili <dbilgili@sabanciuniv.edu>
2019-04-10 22:53:53 +0200

    Make playlist name a link

--- client/src/components/SlidingMenu.js
+++ client/src/components/SlidingMenu.js
@@ -5,13 +5,13 @@ import cross from '../assets/images/cross.png';
 const SlidingMenu = (props) => {
   const { close, exit, playlistData } = props;
 
-  const { spotifyName, pin } = playlistData;
+  const { accessLink, spotifyName, pin } = playlistData;
   return (
     <div className="sliding-menu-container">
       <div className="vertical-wrapper">
         <div className="inner-wrapper">
           <p className="header">Playlist Name</p>
-          <p className="text">{spotifyName}</p>
+          <a className="text" href={accessLink}>{spotifyName}</a>
         </div>
         <div className="inner-wrapper">
           <p className="header">Party PIN</p>
