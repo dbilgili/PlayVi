fabb7f57f1077f8019d30866ee6a010a39004bd4
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2019-04-17 15:12:59 +0300

    Loading bar

---
+++ client/src/components/LoadingBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../assets/stylus/global.css';
+
+const LoadingBar = () => (
+  <div className="linear-progress small">
+    <div className="bar bar1" />
+    <div className="bar bar2" />
+  </div>
+);
+
+export default LoadingBar;
