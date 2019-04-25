933e6b77e0a19c89196551af3625a022ed00366a
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2019-04-25 19:09:17 +0300

    add onChange prop to TextField

--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 const TextInput = (props) => {
-  const { placeholder, getField, validate, pattern, maxlength, alert } = props;
+  const {
+    placeholder,
+    getField,
+    validate,
+    pattern,
+    maxlength,
+    alert,
+    onChange,
+  } = props;
+
   const [input, setInput] = useState('');
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+    onChange();
+  };
 
   useEffect(() => {
     getField(input);
@@ -16,13 +30,14 @@ const TextInput = (props) => {
       pattern={pattern}
       className={alert ? 'custom-text-input alert' : 'custom-text-input'}
       placeholder={placeholder}
-      onChange={e => setInput(e.target.value)}
+      onChange={onChangeHandler}
     />
   );
 };
 
 TextInput.defaultProps = {
   maxLength: 15,
+  onChange: () => {},
 };
 
 export default TextInput;
