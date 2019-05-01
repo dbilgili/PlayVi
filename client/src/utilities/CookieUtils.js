18bc987bd9e08285c2d4d3b2a3afabef5d0eed62
dbilgili <dbilgili@sabanciuniv.edu>
2019-05-01 19:00:04 +0200

    Add other cookie utilities

---
+++ client/src/utilities/CookieUtils.js
@@ -0,0 +1,15 @@
+export const getCookie = () => {
+  const cookieVal = document.cookie.split('=')[1];
+  const headers = { Authorization: cookieVal };
+  return headers;
+};
+
+export const clearCookie = () => {
+  document.cookie = 'SESSION=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+};
+
+export const setCookie = (res) => {
+  document.cookie = `SESSION=${res.headers.authorization}; expires=${new Date(new Date().setFullYear(new Date().getFullYear() + 1))}; path=/`;
+};
+
+export default { getCookie, clearCookie, setCookie };
--- client/src/utilities/GetCookie.js
+++
@@ -1,7 +0,0 @@
-const getCookie = () => {
-  const cookieVal = document.cookie.split('=')[1];
-  const headers = { Authorization: cookieVal };
-  return headers;
-};
-
-export default getCookie;
