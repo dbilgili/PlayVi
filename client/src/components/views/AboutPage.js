64f30cee23d533ada3657b58a5997381e6ec0910
dbilgili <dbilgili@sabanciuniv.edu>
2019-05-09 01:11:21 +0200

    Add back button

--- client/src/components/views/AboutPage.js
+++ client/src/components/views/AboutPage.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import NavigationButtons from '../NavigationButtons';
 
-const AboutPage = () => (
-  <div className="about-page-container">
-    <p>
-      <b>One Night</b>
-      {' '}
+const AboutPage = (props) => {
+  const { screen } = props;
+
+  return (
+    <div className="about-page-container">
+      <p>
+        <b>One Night</b>
+        {' '}
       makes it easy to collaborate on Spotify playlists mainly for home parties where several people get involved and want to have control on the songs to be played during the event.
-    </p>
-    <p>
+      </p>
+      <p>
       Even people who don’t have a Spotify account can still decide what to be played next!
-    </p>
-    <p>
+      </p>
+      <p>
     Just start a party and share the party PIN with people.
-    </p>
-  </div>
-);
+      </p>
+      <NavigationButtons
+        leftButton='Back'
+        backAction={() => screen('frontpage')}
+      />
+    </div>
+  );
+};
 
 export default AboutPage;
