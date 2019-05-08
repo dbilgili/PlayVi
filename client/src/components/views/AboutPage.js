b3f8ae4b018390076c9d88ea31a8911c119e1b16
dbilgili <dbilgili@sabanciuniv.edu>
2019-05-08 23:27:27 +0200

    About page component & styling

---
+++ client/src/components/views/AboutPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div className="about-page-container">
+    <p>
+      <b>One Night</b>
+      {' '}
+      makes it easy to collaborate on Spotify playlists mainly for home parties where several people get involved and want to have control on the songs to be played during the event.
+    </p>
+    <p>
+      Even people who don’t have a Spotify account can still decide what to be played next!
+    </p>
+    <p>
+    Just start a party and share the party PIN with people.
+    </p>
+  </div>
+);
+
+export default AboutPage;
