c4e7d92858771cbd496f740063a576514953d49b
dbilgili <dbilgili@sabanciuniv.edu>
2019-05-08 23:26:57 +0200

    Attach onClick events

--- client/src/components/views/FrontPage.js
+++ client/src/components/views/FrontPage.js
@@ -10,8 +10,8 @@ const FrontPage = (props) => {
       <img alt="logo" className="logo" src={logo} />
       <button type="button" className="big-text-button" onClick={() => screen('create')}>Start a party</button>
       <button type="button" className="big-text-button" onClick={() => screen('join')}>Join to a party</button>
-      <button type="button" className="small-text-button">How to use</button>
-      <button type="button" className="small-text-button second">About</button>
+      <button type="button" className="small-text-button" onClick={() => screen('howto')}>How to use</button>
+      <button type="button" className="small-text-button second" onClick={() => screen('about')}>About</button>
     </div>
   );
 };
