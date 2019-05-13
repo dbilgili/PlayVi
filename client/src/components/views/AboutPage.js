0a08e001ab48629e1784e3dfe07a14181008acaf
dbilgili <dbilgili@sabanciuniv.edu>
2019-05-13 19:32:25 +0200

    Rename app name

--- client/src/components/views/AboutPage.js
+++ client/src/components/views/AboutPage.js
@@ -7,7 +7,7 @@ const AboutPage = (props) => {
   return (
     <div className="about-page-container">
       <p>
-        <b>One Night</b>
+        <b>Playvy</b>
         {' '}
       makes it easy to collaborate on Spotify playlists mainly for home parties where several people get involved and want to have control on the songs to be played during the event.
       </p>
