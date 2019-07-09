b8715a783054a1d39350a19b684ab58edac5606a
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2019-07-10 01:23:14 +0300

    Get rid of the old logo (png)

--- client/src/components/views/FrontPage.js
+++ client/src/components/views/FrontPage.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import logo from '../../assets/images/logo.png';
-
 const FrontPage = (props) => {
   const { screen } = props;
 
   return (
     <div className="front-page-container">
-      <img alt="logo" className="logo" src={logo} />
+      <div className="logo">PlayVi</div>
       <button type="button" className="big-text-button" onClick={() => screen('create')}>Start a party</button>
       <button type="button" className="big-text-button" onClick={() => screen('join')}>Join to a party</button>
-      <button type="button" className="small-text-button" onClick={() => screen('howto')}>How to use</button>
       <button type="button" className="small-text-button second" onClick={() => screen('about')}>About</button>
     </div>
   );
