88a9eb1053b349e28bf5a421c0c7f298363da1a8
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2019-08-10 16:32:19 +0200

    Updates

--- precache-manifest.b4e345dbbafe122752ea00b6c03788a2.js
+++ precache-manifest.b4e345dbbafe122752ea00b6c03788a2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/playvi/static/js/runtime~main.a7ca34ba.js"
   },
   {
-    "revision": "cb608f69edf48781e140",
-    "url": "/playvi/static/js/main.d74badd2.chunk.js"
+    "revision": "e9e92286f991430179f8",
+    "url": "/playvi/static/js/main.5d6b8ae3.chunk.js"
   },
   {
     "revision": "7e34b8796c478728ed21",
     "url": "/playvi/static/js/2.727bc884.chunk.js"
   },
   {
-    "revision": "cb608f69edf48781e140",
-    "url": "/playvi/static/css/main.96933143.chunk.css"
+    "revision": "e9e92286f991430179f8",
+    "url": "/playvi/static/css/main.98bf94cc.chunk.css"
   },
   {
-    "revision": "39536187b842afc56bc9cfdba3ce6ea2",
+    "revision": "3ed1bd9b5bedfc874d30bb57ab41928c",
     "url": "/playvi/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/playvi/precache-manifest.57ea8aea4a5db1d256364f6f098accfe.js"
+  "/playvi/precache-manifest.b4e345dbbafe122752ea00b6c03788a2.js"
 );
 
 workbox.clientsClaim();
